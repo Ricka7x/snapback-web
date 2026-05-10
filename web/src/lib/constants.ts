6c0bae610f4d253b10aa030a7f9164dded76abf8
Ricardo Ramirez <ricky.ramirez89@gmail.com>
2026-05-10 16:29:04 -0600

    chore(release): Snapback v0.29.1
     - Build: 73
     - App archive: Snapback-0.29.1.dmg
     - Built: 2026-05-10 16:29:04

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -1,3 +1,3 @@
-export const LATEST_VERSION = "0.29.0";
+export const LATEST_VERSION = "0.29.1";
 export const DOWNLOAD_URL = `/releases/Snapback-${LATEST_VERSION}.dmg`;
 export const PRO_PURCHASE_URL = "https://snapbackapp.lemonsqueezy.com/buy/snapback-pro";
