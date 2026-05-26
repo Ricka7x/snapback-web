874bbbe6651e479562248509c488609e9a8693b9
Ricardo Ramirez <ricky.ramirez89@gmail.com>
2026-05-26 11:29:21 -0600

    feat: Update Clarity

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -109,7 +109,7 @@ export default function RootLayout({
               c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
               t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
               y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "vxr0eco3jw");
+            })(window, document, "clarity", "script", "wx8zeq826r");
           `}
         </Script>
         <Script
