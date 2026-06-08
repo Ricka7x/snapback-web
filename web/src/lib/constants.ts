99072b705980a1a49b90b2caa7cd212bef429425
Ricardo Ramirez <ricky.ramirez89@gmail.com>
2026-06-08 17:44:12 -0600

    sold-out

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -1,4 +1,4 @@
 export const LATEST_VERSION = "0.33.0";
-export const DISCOUNT_SOLD_OUT = false;
+export const DISCOUNT_SOLD_OUT = true;
 export const DOWNLOAD_URL = `/releases/Snapback-${LATEST_VERSION}.dmg`;
 export const PRO_PURCHASE_URL = "https://snapbackapp.lemonsqueezy.com/buy/snapback-pro";
