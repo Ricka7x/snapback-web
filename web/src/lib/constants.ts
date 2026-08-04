606ae6ed21df4ffdf85770fa4949eba6e16db2cd
Ricardo Ramirez <ricky.ramirez89@gmail.com>
2026-08-04 13:05:47 -0600

    chore(release): Snapback v0.42.2
     - Build: 102
     - App archive: Snapback-0.42.2.dmg
     - Built: 2026-08-04 13:05:47

--- web/src/lib/constants.ts
+++ web/src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const LATEST_VERSION = "0.42.1";
+export const LATEST_VERSION = "0.42.2";
 export const DISCOUNT_SOLD_OUT = true;
 export const DOWNLOAD_URL = `/releases/Snapback-${LATEST_VERSION}.dmg`;
 export const PRO_PURCHASE_URL = "https://store.snapbackapp.com/checkout/buy/83715b0a-448b-4e25-80d2-b2e6203c713e";
